410d32273fc8e882f659a1960a8e2e74aad13967
Victor Micco <victor.oficial093@gmail.com>
2024-04-20 21:57:39 -0300

    fix: remove node_modules of github

--- create-table.js
+++ create-table.js
@@ -1,16 +1,17 @@
 import { sql } from "./db.js";
 
-// sql`DROP TABLE IF EXISTS videos;`.then(() => {
-//   console.log("Table dropped");
-// });
-
-sql`
-    CREATE TABLE videos (
-        id TEXT PRIMARY KEY,
-        title TEXT,
-        description TEXT,
-        duration INTEGER
-    );
-`.then(() => {
-  console.log("Table created");
+sql`DROP TABLE IF EXISTS videos;`.then(() => {
+  console.log("Table dropped");
 });
+
+// sql`
+//     CREATE TABLE videos (
+//         id TEXT PRIMARY KEY,
+//         title TEXT,
+//         description TEXT,
+//         price TEXT,
+//         image TEXT
+//     );
+// `.then(() => {
+//   console.log("Table created");
+// });
